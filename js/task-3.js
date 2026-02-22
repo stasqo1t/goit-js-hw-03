2b50b9de18bf5cc7f8c5c79ab76df0161e172848
Stanislav Skilskyy <skilskyy.stas@gmail.com>
2026-02-22 22:47:18 +0100

    hw-03

---
+++ js/task-3.js
@@ -0,0 +1,25 @@
+function filterArray(numbers, value) {
+  const result = [];
+
+  for (let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] > value) {
+      result.push(numbers[i]);
+    }
+  }
+
+  return result;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); 
+
+
+console.log(filterArray([1, 2, 3, 4, 5], 4)); 
+
+
+console.log(filterArray([1, 2, 3, 4, 5], 5)); 
+
+
+console.log(filterArray([12, 24, 8, 41, 76], 38)); 
+
+
+console.log(filterArray([12, 24, 8, 41, 76], 20)); 
